3ef8b775393143674fd7b38165589f02816bca91
yusra200912 <yusrafatima0609@gmail.com>
2025-10-25 19:18:26 +0500

    .

---
+++ python/lesson 6.py/hexagon.py
@@ -0,0 +1,23 @@
+import turtle
+turtle.Screen().bgcolor("yellow")
+
+board= turtle.Turtle()
+board.forward(50)
+
+
+board.left(60)
+board.forward(50)
+
+board.left(60)
+board.forward(50)
+
+board.left(60)
+board.forward(50)
+
+board.left(60)
+board.forward(50)
+
+board.left(60)
+board.forward(50)
+
+
