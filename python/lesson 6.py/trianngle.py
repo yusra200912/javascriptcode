02664dc034467122969ff52f24eb36849fb74999
yusra200912 <yusrafatima0609@gmail.com>
2025-10-25 18:37:38 +0500

    mm

---
+++ python/lesson 6.py/trianngle.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+
+board= turtle.Turtle()
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+turtle.done()
