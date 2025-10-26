a790ce2b09b0d4bd41e59bc7069c99c017e70b59
yusra200912 <yusrafatima0609@gmail.com>
2025-10-26 18:18:58 +0500

    .

---
+++ python/lesson 6.py/conditions in pyhton.py/activity 1.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number"))
+print("Number to be checked :", number)
+
+if number<0:
+    print("Number is negative")
+elif number>0:
+    print("Number is positive")
+else:
+    print("Number is neutral") 
